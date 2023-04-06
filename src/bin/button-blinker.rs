ad2014b380ce17ec49c019e171452467f98043a5
Brooks J Rady <b.j.rady@gmail.com>
2023-04-06 23:24:40 +0100

    Improve the button blinker

--- src/bin/button-blinker.rs
+++ src/bin/button-blinker.rs
@@ -6,14 +6,14 @@ use core::sync::atomic::{AtomicU32, Ordering};
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_rp::{
-    gpio::{Input, Level, Output, Pull},
-    peripherals::{PIN_13, PIN_15},
-};
+use embassy_futures::select::select;
+use embassy_rp::gpio::{AnyPin, Input, Level, Output, Pin, Pull};
+use embassy_sync::signal::Signal;
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
 
 static BLINK_FREQ: AtomicU32 = AtomicU32::new(1);
+static FREQ_CHANGED: Signal<()> = Signal::new();
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
@@ -25,36 +25,47 @@ async fn main(spawner: Spawner) {
 
     // Set up the other two LEDs (one on, one off)
     // It would be nice to see if I could squish these into an array...
-    let led_a = Output::new(p.PIN_13, Level::High);
-    let led_b = Output::new(p.PIN_15, Level::Low);
+    let leds = [
+        Output::new(p.PIN_13.degrade(), Level::High),
+        Output::new(p.PIN_15.degrade(), Level::Low),
+    ];
 
     // Set up a pull-down button
     let mut button = Input::new(p.PIN_14, Pull::Down);
 
     // Spawn the blinker task
-    unwrap!(spawner.spawn(blinker(led_a, led_b)));
+    unwrap!(spawner.spawn(blinker(leds)));
 
     // Loop
     loop {
         // Wait for a the button to be pressed...
         button.wait_for_falling_edge().await;
 
-        // Update blinker frequency
-        let new_freq = BLINK_FREQ.load(Ordering::SeqCst) % 10 + 1;
+        // Update blinker frequency (double and wrap after 64 Hz)
+        let new_freq = BLINK_FREQ.load(Ordering::SeqCst) * 2 % 127;
         BLINK_FREQ.store(new_freq, Ordering::SeqCst);
-        info!("Updated frequency: {}", new_freq);
+        info!("Updated frequency: {} Hz", new_freq);
+
+        // Signal the the blinker to update frequency immediately, skipping the remainder of it's
+        // current delay and continuing the loop
+        FREQ_CHANGED.signal(());
     }
 }
 
 #[embassy_executor::task]
-async fn blinker(mut led_a: Output<'static, PIN_13>, mut led_b: Output<'static, PIN_15>) {
+async fn blinker(mut leds: [Output<'static, AnyPin>; 2]) {
     loop {
         // Swap LED states
-        led_a.toggle();
-        led_b.toggle();
+        for led in &mut leds {
+            led.toggle();
+        }
 
-        // Wait 1s
+        // Wait an amount of time or for the frequency to be updated
         let delay = 1000 / BLINK_FREQ.load(Ordering::SeqCst);
-        Timer::after(Duration::from_millis(delay.into())).await;
+        select(
+            Timer::after(Duration::from_millis(delay.into())),
+            FREQ_CHANGED.wait(),
+        )
+        .await;
     }
 }
