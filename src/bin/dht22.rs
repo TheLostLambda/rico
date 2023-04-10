79fd62591a5e8f8ff5897a976f8337b0931a84d6
Brooks J Rady <b.j.rady@gmail.com>
2023-04-10 17:49:25 +0100

    Working DHT22 example

--- src/bin/dht22.rs
+++ src/bin/dht22.rs
@@ -5,7 +5,7 @@
 use defmt::*;
 use dht_sensor::*;
 use embassy_executor::Spawner;
-use embassy_rp::gpio::{Flex, Level, OutputOpenDrain, Pull};
+use embassy_rp::gpio::{Level, OutputOpenDrain};
 use embassy_time::{Delay, Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
 
@@ -14,28 +14,16 @@ async fn main(_spawner: Spawner) {
     // Initialise Peripherals
     let p = embassy_rp::init(Default::default());
 
-    // Err, do I need this?
-    let mut delay = Delay;
-
     // Pull the pin high for initialisation
     let mut sensor_pin = OutputOpenDrain::new(p.PIN_12, Level::High);
-    // let mut sensor_pin = Flex::new(p.PIN_12);
-    // sensor_pin.set_pull(Pull::None);
-    // sensor_pin.set_as_output();
-    // sensor_pin.set_high();
 
-    loop {
-        // Wait for the sensor to initialise...
-        info!("Waiting on the sensor...");
-        Timer::after(Duration::from_secs(1)).await;
-        sensor_pin.set_low();
-        Timer::after(Duration::from_millis(18)).await;
-        sensor_pin.set_high();
-    }
+    // Wait for the sensor to initialise...
+    info!("Waiting on the sensor...");
+    Timer::after(Duration::from_secs(1)).await;
 
     // Loop
     loop {
-        match dht22::Reading::read(&mut delay, &mut sensor_pin) {
+        match dht22::Reading::read(&mut Delay, &mut sensor_pin) {
             Ok(dht22::Reading {
                 temperature,
                 relative_humidity,
@@ -46,6 +34,7 @@ async fn main(_spawner: Spawner) {
                 DhtError::Timeout => info!("Timeout!"),
             },
         }
+
         // Wait 1s
         Timer::after(Duration::from_secs(1)).await;
     }
